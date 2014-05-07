e6f44e29f669c4bbb94f92173ade69cdb1bd88a2
Steve Buckley <buckleys78@gmail.com>
2014-05-07 14:06:02 -0700

    got rake working, relocated index.html

--- test/features/can_access_home_test.rb
+++ test/features/can_access_home_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 feature "CanAccessHome" do
   scenario "the test is sound" do
     visit root_path
-    page.must_have_content "Hello World"
-    page.wont_have_content "Goobye All!"
+    #page.must_have_content "Hello World"
+    page.must_have_content "Steve Buckley"
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -22,9 +22,9 @@ class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
 end
 
-class ActionDispatch::IntegrationTest
-  include Capybara::DSL
-  include Capybara::Assertions
-end
+#class ActionDispatch::IntegrationTest
+ # include Capybara::DSL
+  #include Capybara::Assertions
+#end
 
 
