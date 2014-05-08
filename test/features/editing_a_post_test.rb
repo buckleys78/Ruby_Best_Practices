9efe4b851b1ccbba2dc27d4e02873ad5166b0709
Steve Buckley <buckleys78@gmail.com>
2014-05-08 15:50:18 -0700

    designing CheckForZurb test.

--- test/features/editing_a_post_test.rb
+++ test/features/editing_a_post_test.rb
@@ -4,7 +4,7 @@ feature "Editing A Post" do
   scenario "submit updates to an existing post" do
     # Given an existing post
     post = Post.create(title: "Becoming a Code Fellow",
-                                    body: "Means striving for excellence.")
+                       body: "Means striving for excellence.")
     visit post_path(post)
 
     # When I click edit and submit changed data
