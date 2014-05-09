2aa6b07fdbf263eca706a761372b45cfe2b80f3a
Steve Buckley <buckleys78@gmail.com>
2014-05-08 18:58:23 -0700

    Theme added, fixing links.

--- test/features/check_for_zurb_test.rb
+++ test/features/check_for_zurb_test.rb
@@ -3,9 +3,12 @@ require "test_helper"
 feature "Check For Zurb gem" do
   scenario "Zurb implemented" do
     #Given that Zurb has been employed,
-    visit root_path
+
     #When the homepage is visited,
-    page.html.must_include "foundation_and_overrides.css"
+    visit root_path
+
     #Then a stylesheet with Zurb in the name is found.
+    page.html.must_include "foundation_and_overrides.css"
+
   end
 end
