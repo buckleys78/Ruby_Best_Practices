cef891678622a7975f4e99389c39a756fa5d3a14
Steve Buckley <buckleys78@gmail.com>
2014-05-13 19:40:27 -0700

    safe spot

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,4 @@
 class Project < ActiveRecord::Base
-  validates :name, length: { in: 4..255 }, uniqueness: true
+  validates :name, length: { in: 4..255 }
   validates :technologies_used, presence: true
 end
